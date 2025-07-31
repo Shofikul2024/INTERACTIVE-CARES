e44d9b9917d9a243aecfc669a94f5ca278027bdd
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-31 23:46:18 +0600

    added tuple

---
+++ class-3-practice-shofikul/string-example.py
@@ -0,0 +1,15 @@
+# 1. Strings
+#     - Single Quotes
+#     - Double Quotes
+#     - Triple Single Quotes
+#     - Double Single Quotes
+
+#
+# text='hello world'
+# text[0]='a'
+# print(text)
+
+text= "Hello World"
+text[0]="A"
+print(text)
+
---
+++ class-3-practice-shofikul/todo.py
@@ -0,0 +1,59 @@
+#
+# 1. Strings
+#     - Single Quotes
+#     - Double Quotes
+#     - Triple Single Quotes
+#     - Double Single Quotes
+#
+# 2. String Indexing
+#     - Positive Indexing
+#     - Negative Indexing
+#
+# 3. String Slicing
+#
+# 4. String Concatenation
+#
+# 5. String Method
+#     - String Repetition
+#     - Formate Using %
+#     - uppercase
+#     - lowercase
+#     - Capitalize
+#     - Title case
+#
+# 6. String Replacement
+# 7. String Split into List
+#
+# 8. String tripping - removing white space
+#     - lstrip
+#     - rstrip
+#     - strip
+#
+# 9. Math or Number Arithmetic calculation
+#     - Addition
+#     - Subtraction
+#     - Multiplication
+#     - Division
+#     - Modulus
+#
+#
+# 10. Type Conversion
+#     - Int->Float
+#     - Float->Int
+#     - []->String
+#
+# 11. Python Operator Precedence
+#     - Multiplication (*)
+#     - Division (/)
+#     - Modulus (%)
+#     - Addition (+)
+#     - Subtraction (-)
+#
+# 12. Python Math
+#     - Square root
+#     - Power
+#     - Trigonometric functions
+#     - Logarithmic
+#     - Factorial
+#     - Greatest common divisor
+#
