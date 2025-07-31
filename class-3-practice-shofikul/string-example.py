6b6a9ccab1c26276818aaf4a29bf9eb24ec2e047
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-31 23:51:43 +0600

    added tuple

--- class-3-practice-shofikul/string-example.py
+++ class-3-practice-shofikul/string-example.py
@@ -4,12 +4,43 @@
 #     - Triple Single Quotes
 #     - Double Single Quotes
 
-#
+
+'''  aita vul ace karon string  ar value
+        inddex  diye poriborton kora jaina
+
+        string  immutable   , tai   output error
+
+asbe '''
+
 # text='hello world'
 # text[0]='a'
 # print(text)
 
-text= "Hello World"
-text[0]="A"
-print(text)
 
+
+
+
+'''  aita vul ace karon string  ar value 
+        inddex  diye poriborton kora jaina 
+   
+        string  immutable   , tai   output error   
+
+asbe '''
+
+
+#
+# text= "Hello World"
+# text[0]="A"
+# print(text)
+
+
+
+
+#
+#
+# name='shofikul'
+# demo="shofikul"
+# demo1='''shofikul'''
+# print(name)
+# print(demo)
+# print(demo1)
